e4d408f190ce9f9c0b96494ca9b43ba6f0af717a
Max <nnpctons@gmail.com>
2025-10-05 19:30:47 +0300

    Raffle update 19-29 5-10-2025

--- UsersPortalTests/UsersPortalTests/UsersPortalTests.cs
+++ UsersPortalTests/UsersPortalTests/UsersPortalTests.cs
@@ -19,16 +19,50 @@ namespace UsersPortalTests
         [Retry(2)]
         [Author("Maksim", "maxqatesting390@gmail.com")]
         [AllureSuite("Positive critical scenarios general portal")]
-        [AllureSubSuite("Sign_In_Log_Out_Success")]
-        public void Sign_In_Log_Out_Success()
+        [AllureSubSuite("Sign_In_Without_Phone_Success")]
+        public void Sign_In_Without_Phone_Success()
         {
+            //Flow: Sign in via sidebar > Verify > full user name, user email > Verify full user name on sidebar , user email on My details page
+            #region Test data
+
+
+
+            #endregion
+
+            #region Preconditions API
+
+
+
+            #endregion
+
+            #region Preconditions DB
+
+
+
+            #endregion
+
+            #region Preconditions GUI
+
             PagesUsersPortals
                 .WeUseCookiesMdlWnd.ClickButtonGotIt();
             PagesUsersPortals
                 .Header.ClickButtonHamburger();
 
+            #endregion
+
+            #region Test GUI
+
+
+
+            #endregion
+
+            #region Postconditions
+
+
+
+            #endregion
+
             WaitUntil.WaitSomeInterval(2000);
         }
     }
-
 }
